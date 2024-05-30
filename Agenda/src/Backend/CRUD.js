9748745f66057890d82e28b446b2e789822d1293
Marcos Garcia <marcosfabricio633@gmail.com>
2024-05-29 23:40:51 -0300

    New CRUD

--- Agenda/src/Backend/CRUD.js
+++ Agenda/src/Backend/CRUD.js
@@ -1,76 +1,86 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const fs = require('fs');
+const bodyParser = require('body-parser');
 
 const appEx = express();
-const PORT = 3000;
-
 appEx.use(bodyParser.json());
-appEx.use(express.static('public'));
 
-const dataFilePath = './data.json';
-
-// Configuración de CORS
-appEx.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
-  
 // READ DATA
-function readData() {
-    if (fs.existsSync(dataFilePath)) {
-        const data = fs.readFileSync(dataFilePath);
-        return JSON.parse(data);
-    } else {
-        return [];
+const readData = () => {
+    try{
+        const data = fs.readFileSync("./data.json");
+        return (JSON.parse(data));
+    } catch(error) {
+        console.log(error);
     }
-}
+};
 
-// WRITE DATA
-function writeData(data) {
-    fs.writeFileSync(dataFilePath, JSON.stringify(data, null, 2));
-}
-
-// CREATE DATA
-appEx.post('/items', (req, res) => {
-    const items = readData();
-    const newItem = { id: Date.now(), ...req.body };
-    items.push(newItem);
-    writeData(items);
-    res.status(201).json(newItem);
-});
-
-// READ
-appEx.get('/items', (req, res) => {
-    const items = readData();
-    res.json(items);
-});
-
-// UPDATE
-appEx.put('/items/:id', (req, res) => {
-    const items = readData();
-    const index = items.findIndex(item => item.id === parseInt(req.params.id));
-
-    if (index !== -1) {
-        items[index] = { ...items[index], ...req.body };
-        writeData(items);
-        res.json(items[index]);
-    } else {
-        res.status(404).json({ message: 'item not found' });
+// WRITHE DATA
+writeData = (data) =>{
+    try{
+        fs.writeFileSync("./data.json", JSON.stringify(data));
+    } catch(error) {
+        console.log(error);
     }
+};
+
+// Endpoints
+appEx.get("/", (req, res) => {
+    res.send("hola, esto es la raiz");
 });
 
-// DELET
-appEx.delete('/items/:id', (req, res) => {
-    let items = readData();
-    items = items.filter(item => item.id !== parseInt(req.params.id));
-    writeData(items);
-    res.status(204).end();
+// OBTENER TODAS LAS FICHAS
+appEx.get("/fichas", (req, res) => {
+    const data = readData();
+    res.json(data.fichas);
 });
 
+// OBTENER UNA FICHA CON EL ID
+appEx.get("/fichas/:id", (req, res) => {
+    const data = readData();
+    const id = parseInt(req.params.id);
+    const ficha = data.fichas.find((ficha) => ficha.id === id);
+    res.json(ficha);
+});
 
-appEx.listen(PORT, () => {
-    console.log(`server is running on http://localhost:${PORT}`);
+// CREAR UNA FICHA NUEVA
+appEx.post("/fichas", (req, res) => {
+    const data = readData();
+    const body = req.body;
+    const newFicha = {
+        id: data.fichas.length + 1,
+        ...body,
+    };
+    data.fichas.push(newFicha);
+    writeData(data);
+    res.json(newFicha);
+});
+
+// ACTUALIZAR FICHA
+appEx.put("/fichas/:id", (req, res) => {
+    const data = readData();
+    const body = req.body;
+    const id = parseInt(req.params.id);
+    const fichaIndex = data.fichas.findIndex((ficha) => ficha.id === id);
+    data.fichas[fichaIndex] = {
+        ...data.fichas[fichaIndex],
+        ...body,
+    };
+    writeData(data);
+    res.json({ message: "Ficha actualizada"});
+});
+
+// ELIMINAR FICHA
+appEx.delete("/fichas/:id", (req, res) => {
+    const data = readData();
+    const id = parseInt(req.params.id);
+    const fichaIndex = data.fichas.findIndex((ficha) => ficha.id === id);
+    data.fichas.splice(fichaIndex, 1);
+    writeData(data);
+    res.json({message: "Ficha eliminada"})
+});
+
+// ESCUCHANDO SERVIDOR
+appEx.listen(3000, () => {
+    console.log('SERVER LISTENING ON PORT 3000');
 });
